0b237cb53aa8b5a62851203b0388aa3bc2eb8114
Woudini <woudini@woudinis-mini.attlocal.net>
2014-11-14 18:27:45 -0600

    Initial Commit

---
+++ GameOfSkate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GameOfSkate
+//
+//  Created by Woudini on 11/14/14.
+//  Copyright (c) 2014 Hi Range. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GameOfSkate/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  GameOfSkate
+//
+//  Created by Woudini on 11/14/14.
+//  Copyright (c) 2014 Hi Range. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
